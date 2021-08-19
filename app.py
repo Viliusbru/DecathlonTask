e7ef8264cecb3a9c512f9dd9a112ec649cda2943
Viliusbru <viliusbru@gmail.com>
2021-08-19 12:17:22 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask.helpers import flash
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+import csv
+import os
+from main import jsonify, read_csv, Athlete
+
+
+app = Flask(__name__)
+app.config['FLASK_ENV'] = 'development'
+app.config['DEBUG'] = True
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+      if request.method == 'POST':
+         file = request.files['file']
+         if file.filename != '':
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+            read_csv(file_path)
+      return redirect(url_for('index'))
+
+if __name__ == '__main__':
+   app.run(debug=True)
+
+
+# python -m flask run
---
+++ main.py
@@ -0,0 +1,45 @@
+import csv
+
+objects = []
+data_list = []
+filename = 'data.csv'
+class Athlete:
+    def __init__(self, name, hundred_meters, long_jump, shot_put, high_jump, fourhundred_meters, hurdles, discus_throw, pole_vault, javelin_throw, fifteenhundred_meters):
+        self.name = name
+        self.hundred_meters = hundred_meters
+        self.long_jump = long_jump
+        self.shot_put = shot_put
+        self.high_jump = high_jump
+        self.fourhundred_meters = fourhundred_meters
+        self.hurdles = hurdles
+        self.discus_throw = discus_throw
+        self.pole_vault = pole_vault
+        self.javelin_throw = javelin_throw
+        self.fifteenhundred_meters = fifteenhundred_meters
+
+def read_csv(filename):
+    with open(filename, 'r',) as f:
+        reader = csv.reader(f, delimiter=';')
+        for row in reader:
+            data_list.append(row)
+
+    for i in range(len(data_list)):
+        name = data_list[i][0]
+        hundred_meters = data_list[i][1]
+        long_jump = data_list[i][2]
+        shot_put = data_list[i][3]
+        high_jump = data_list[i][4]
+        fourhundred_meters = data_list[i][5]
+        hurdles = data_list[i][6]
+        discus_throw = data_list[i][7]
+        pole_vault = data_list[i][8]
+        javelin_throw = data_list[i][9]
+        fifteenhundred_meters = data_list[i][10]
+        objects.append(Athlete(name, hundred_meters, long_jump, shot_put, high_jump, fourhundred_meters, hurdles, discus_throw, pole_vault, javelin_throw, fifteenhundred_meters)) 
+
+read_csv(filename)
+
+def jsonify():
+    for object in objects:
+        print(object.name, object.hundred_meters)
+jsonify()
